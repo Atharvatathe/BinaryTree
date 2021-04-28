79a69647d4bcdc9e79be96b1af37fb7c9bce346b
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-28 21:07:17 +0530

    Added: Create Binary Tree of Multiple Node

--- test/java/MyBinaryTreeTest.java
+++ test/java/MyBinaryTreeTest.java
@@ -9,8 +9,18 @@ public class MyBinaryTreeTest {
         myBinaryTree.add(56);
         myBinaryTree.add(30);
         myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
 
         int size =myBinaryTree.getSize();
-        Assertions.assertEquals(3,size);
+        Assertions.assertEquals(13,size);
     }
 }
